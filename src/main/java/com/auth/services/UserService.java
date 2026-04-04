821b5521e113d820251d12904d10a7fb78b007da
Kritiman Singh <kritimansingh123@gmail.com>
2026-04-04 23:20:54 +0530

    setting Repos

---
+++ src/main/java/com/auth/services/UserService.java
@@ -0,0 +1,28 @@
+package com.auth.services;
+
+import com.auth.dtos.UserDto;
+
+public interface UserService {
+
+    //create user
+    UserDto createUser(UserDto userDto);
+
+    //get user by email
+    UserDto getUserByEmail(String email);
+
+    //update user
+    UserDto updateUser(UserDto userDto, String userId);
+
+    //delete user
+    void deleteUser(String userId);
+
+    //get user by id
+    UserDto getUserById(String userId);
+
+    //get all users
+    Iterable<UserDto> getAllUsers();
+
+    // user service se related __
+
+
+}
