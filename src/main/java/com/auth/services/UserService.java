91324cea8c7e145e0eac3cffdc304520b8bb74bb
Kritiman Singh <kritimansingh123@gmail.com>
2026-04-05 11:26:40 +0530

    adding setup for users

--- src/main/java/com/auth/services/UserService.java
+++ src/main/java/com/auth/services/UserService.java
@@ -2,13 +2,13 @@ package com.auth.services;
 
 import com.auth.dtos.UserDto;
 
+
 public interface UserService {
 
     //create user
     UserDto createUser(UserDto userDto);
 
     //get user by email
-/*
     UserDto getUserByEmail(String email);
 
     //update user
@@ -22,7 +22,7 @@ public interface UserService {
 
     //get all users
     Iterable<UserDto> getAllUsers();
-*/
+
 
     // user service se related __
 
--- src/main/java/com/auth/services/impl/UserServiceImpl.java
+++ src/main/java/com/auth/services/impl/UserServiceImpl.java
@@ -10,8 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 
 
+@Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
@@ -21,6 +23,8 @@ public class UserServiceImpl implements UserService {
 
     //private final RoleRepository roleRepository;
 
+    @Override
+    @Transactional
     public UserDto createUser(UserDto userDto) {
         if (userDto.getEmail() == null || userDto.getEmail().isBlank()) {
             throw new IllegalArgumentException("Email is required");
@@ -42,6 +46,56 @@ public class UserServiceImpl implements UserService {
 
         User savedUser = userRepository.save(user);
         return modelMapper.map(savedUser, UserDto.class);
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with given email id "));
+        return modelMapper.map(user, UserDto.class);
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, String userId) {
+        UUID uId = UserHelper.parseUUID(userId);
+        User existingUser = userRepository
+                .findById(uId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with given id"));
+        //we are not going to change email id for this project.
+        if (userDto.getName() != null) existingUser.setName(userDto.getName());
+        if (userDto.getImage() != null) existingUser.setImage(userDto.getImage());
+        if (userDto.getProvider() != null) existingUser.setProvider(userDto.getProvider());
+        //TODO: change password updation logic...
+        if (userDto.getPassword() != null) existingUser.setPassword(userDto.getPassword());
+        existingUser.setEnable(userDto.isEnable());
+        existingUser.setUpdatedAt(Instant.now());
+        User updatedUser = userRepository.save(existingUser);
+        return modelMapper.map(updatedUser, UserDto.class);
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        UUID uId = UserHelper.parseUUID(userId);
+        User user = userRepository.findById(uId).orElseThrow(() -> new ResourceNotFoundException("User not found with given id"));
+        userRepository.delete(user);
+    }
+
+    @Override
+    public UserDto getUserById(String userId) {
+        User user = userRepository.findById(UserHelper.parseUUID(userId)).orElseThrow(() -> new ResourceNotFoundException("User not found with given id"));
+        return modelMapper.map(user, UserDto.class);
+    }
+
+    @Override
+    @Transactional
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository
+                .findAll()
+                .stream()
+                .map(user -> modelMapper.map(user, UserDto.class))
+                .toList();
     }
 
 }
