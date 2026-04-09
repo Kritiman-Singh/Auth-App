7d196153c2bbde65bfb845f248ffdf95c466575b
Kritiman Singh <kritimansingh123@gmail.com>
2026-04-09 22:26:13 +0530

    Adding Security

--- src/main/java/com/auth/config/SecurityConfig.java
+++ src/main/java/com/auth/config/SecurityConfig.java
@@ -1,9 +1,19 @@
 package com.auth.config;
 
 
+import com.auth.entities.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService users(){
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+      UserDetails user1 =  userBuilder.username("raj").password("123").roles("ADMIN").build();
+    }
 
 }
