d11490e65a5ed7e2f733dc7a2d5a02b8ba9d5740
Kritiman Singh <kritimansingh123@gmail.com>
2026-04-13 22:39:35 +0530

    Adding JWT Configurations

--- src/main/java/com/auth/services/impl/JwtService.java
+++ src/main/java/com/auth/services/impl/JwtService.java
@@ -2,6 +2,14 @@ package com.auth.services.impl;
 
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+
 @Service
 public class JwtService {
+
+    private final SecretKey secretKey;
+
+    public JwtService(SecretKey secretKey) {
+        this.secretKey = secretKey;
+    }
 }
